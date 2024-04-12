f7b96b53a8743b9f13cab35d3603849b28c754ef
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2024-04-12 10:30:16 -0300

    Update UserSchema to match UserDocument interface requirements by mongoose

--- src/models/User.ts
+++ src/models/User.ts
@@ -11,7 +11,7 @@ export interface UserDocument extends Document {
 	updatedAt: Date;
 }
 
-const UserSchema: Schema = new Schema({
+const UserSchema = new Schema<UserDocument>({
 	name: { type: String, required: true },
 	email: { type: String, required: true, unique: true },
 	password: { type: String, required: true },
