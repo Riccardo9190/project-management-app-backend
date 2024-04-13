16ea0ea0c84b4a99700675bf2997cee154e54c5c
Ricardo Bezerra <ricardo.waliton2@gmail.com>
2024-04-13 10:22:25 -0300

    change project architecture

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,13 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import connectToDatabase from "./config/database";
+import connectToDatabase from "../config/database";
+import { authRouter } from "./routes";
 
 const app = express();
 
 app.use(express.json());
+app.use("/auth", authRouter);
 
 const PORT = process.env.PORT || 3000;
 
